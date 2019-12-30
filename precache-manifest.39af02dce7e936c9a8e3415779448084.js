07219d4d71fdd581a63c8821663eb025d148e5ab
Joji Jacob <joji.jacob.k@gmail.com>
2019-12-30 16:47:12 +0530

    Updates

---
+++ precache-manifest.39af02dce7e936c9a8e3415779448084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b42d22008fbd0c71b7bc01d1a227d3",
+    "url": "/AWS-Instance-Remote/index.html"
+  },
+  {
+    "revision": "37722150b20b134dfa28",
+    "url": "/AWS-Instance-Remote/static/css/main.f141b893.chunk.css"
+  },
+  {
+    "revision": "4ee796cfabedc5bd953d",
+    "url": "/AWS-Instance-Remote/static/js/2.cd5ddd3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AWS-Instance-Remote/static/js/2.cd5ddd3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37722150b20b134dfa28",
+    "url": "/AWS-Instance-Remote/static/js/main.935bad2f.chunk.js"
+  },
+  {
+    "revision": "954c64eba8c029211567",
+    "url": "/AWS-Instance-Remote/static/js/runtime-main.ea83cff0.js"
+  }
+]);
